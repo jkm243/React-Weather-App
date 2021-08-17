4e0f5d1dd92f72d625f5e880bc89c7b01056c8f2
Jacques Katsuva <jacqueskatsuva@gmail.com>
2021-08-18 02:39:22 +0300

    essai

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import logo from './w-logo.svg';
 import './App.css';
-//import { Card } from 'semantic-ui-react'
+import { Card } from 'semantic-ui-react'
 // importer semantic et travailler
 
 
